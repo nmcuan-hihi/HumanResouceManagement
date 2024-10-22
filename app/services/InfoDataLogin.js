292ad2846762153fa19fa4e4de3f757f892a064a
nmcuan-hihi <nguyennnminhhhquannn@gmail.com>
2024-10-23 00:49:52 +0700

    up file

---
+++ app/services/InfoDataLogin.js
@@ -0,0 +1,35 @@
+
+import {
+    getDatabase,
+    ref,
+    set,
+    get,
+    child,
+    update,
+    remove,
+  } from "firebase/database";
+  import { app } from "../config/firebaseconfig";
+  import { initializeApp } from "firebase/app";
+  import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from "firebase/storage";
+  const database = getDatabase(app);
+  const storage = getStorage(app); // Khởi tạo Firebase Storage
+
+// Hàm lấy thông tin nhân viên theo ID
+export const getEmployeeById = async (employeeId) => {
+    try {
+        const dbRef = ref(database);
+        const snapshot = await get(child(dbRef, `employees/${employeeId}`)); // Lấy dữ liệu từ node 'employees/{employeeId}'
+    
+        if (snapshot.exists()) {
+          const employee = snapshot.val(); // Lấy dữ liệu và chuyển đổi thành đối tượng      
+         
+          return employee.name; // Trả về thông tin nhân viên
+        } else {
+          console.log("No employee found with ID:", employeeId);
+          return null; // Không tìm thấy nhân viên
+        }
+      } catch (error) {
+        console.error("Error reading employee:", error);
+      }
+  };
+  
