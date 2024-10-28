3c65b2b8148faebe0b786eb6fc2ae81dab517014
nmcuan-hihi <nguyennnminhhhquannn@gmail.com>
2024-10-29 00:20:22 +0700

    ok

--- app/Screen/EmployeeScreen/EditEmployee.jsx
+++ app/Screen/EmployeeScreen/EditEmployee.jsx
@@ -9,6 +9,7 @@ import {
   TouchableOpacity,
   Image,
   Alert,
+  Modal,
 } from "react-native";
 import {
   readEmployees,
@@ -16,13 +17,12 @@ import {
   toggleEmployeeStatus,
   readPhongBan1,
   readChucVu,
-  
   editPhongBan,
 } from "../../services/database";
-import { Picker } from "@react-native-picker/picker";
 import RNPickerSelect from "react-native-picker-select";
 import BackNav from "../../Compoment/BackNav";
 import { readEmployeesFireStore, getEmployeeById } from "../../services/EmployeeFireBase";
+import { Calendar } from "react-native-calendars";
 
 export default function EmployeeEditScreen({ navigation, route }) {
   const { manv } = route.params;
@@ -129,135 +129,174 @@ export default function EmployeeEditScreen({ navigation, route }) {
   }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scrollView}>
-        <View style={styles.avatarContainer}>
-          <Image
-            source={
-              employeeData.imageURL
+    <><View style={styles.header}>
+      <BackNav
+        navigation={navigation}
+        name={"Chỉnh Sửa"}
+        btn={"Sửa"}
+        onEditPress={handleUpdate}
+
+        />
+    </View><SafeAreaView style={styles.container}>
+        <ScrollView contentContainerStyle={styles.scrollView}>
+          <View style={styles.avatarContainer}>
+            <Image
+              source={employeeData.imageURL
                 ? { uri: employeeData.imageURL }
-                : require("../../../assets/image/images.png")
-            }
-            style={styles.avatar}
-          />
-        </View>
-        <RNPickerSelect
-          onValueChange={(itemValue) => updateField("phongbanId", itemValue)}
-          items={phongBans.map((phong) => ({
-            label: phong.tenPhongBan,
-            value: phong.maPhongBan,
-          }))}
-          value={employeeData.phongbanId}
-          placeholder={{ label: "Chọn phòng", value: "" }}
-          style={pickerSelectStyles}
-        />
+                : require("../../../assets/image/images.png")}
+              style={styles.avatar} />
+          </View>
+          <RNPickerSelect
+            onValueChange={(itemValue) => updateField("phongbanId", itemValue)}
+            items={phongBans.map((phong) => ({
+              label: phong.tenPhongBan,
+              value: phong.maPhongBan,
+            }))}
+            value={employeeData.phongbanId}
+            placeholder={{ label: "Chọn phòng", value: "" }}
+            style={pickerSelectStyles} />
 
-        <RNPickerSelect
-          onValueChange={(itemValue) => updateField("chucvuId", itemValue)}
-          items={Object.entries(chucVus).map(([key, value]) => ({
-            label: value.loaichucvu,
-            value: key,
-          }))}
-          value={employeeData.chucvuId}
-          placeholder={{ label: "Chọn chức vụ", value: "" }}
-          style={pickerSelectStyles}
-        />
+          <RNPickerSelect
+            onValueChange={(itemValue) => updateField("chucvuId", itemValue)}
+            items={Object.entries(chucVus).map(([key, value]) => ({
+              label: value.loaichucvu,
+              value: key,
+            }))}
+            value={employeeData.chucvuId}
+            placeholder={{ label: "Chọn chức vụ", value: "" }}
+            style={pickerSelectStyles} />
 
-        <InputField
-          label="Mã Nhân Viên"
-          value={employeeData.employeeId}
-          onChangeText={(value) => updateField("employeeId", value)}
-        />
-        <InputField
-          label="Họ Tên"
-          value={employeeData.name}
-          onChangeText={(value) => updateField("name", value)}
-        />
-        <InputField
-          label="Mật khẩu"
-          value={employeeData.matKhau}
-          onChangeText={(value) => updateField("matKhau", value)}
-        />
+          <InputField
+            label="Mã Nhân Viên"
+            value={employeeData.employeeId}
+            onChangeText={(value) => updateField("employeeId", value)} />
+          <InputField
+            label="Họ Tên"
+            value={employeeData.name}
+            onChangeText={(value) => updateField("name", value)} />
+          <InputField
+            label="Mật khẩu"
+            value={employeeData.matKhau}
+            onChangeText={(value) => updateField("matKhau", value)} />
 
-        <TouchableOpacity onPress={() => showCalendarModal("ngaysinh")} style={styles.datePicker}>
-          <Text style={styles.label}>Ngày sinh</Text>
-          <Text>{employeeData.ngaysinh || "Chọn ngày"}</Text>
-        </TouchableOpacity>
+          <TouchableOpacity onPress={() => showCalendarModal("ngaysinh")} style={styles.datePicker}>
+            <Text style={styles.label}>Ngày sinh</Text>
+            <Text>{employeeData.ngaysinh || "Chọn ngày"}</Text>
+          </TouchableOpacity>
 
-        <RNPickerSelect
-          value={employeeData.gioitinh}
-          onValueChange={(value) => updateField("gioitinh", value)}
-          items={[
-            { label: "Nam", value: "Nam" },
-            { label: "Nữ", value: "Nữ" },
-            { label: "Khác", value: "Khác" },
-          ]}
-          placeholder={{ label: "Chọn giới tính", value: "" }}
-          style={pickerSelectStyles}
-        />
+          <RNPickerSelect
+            value={employeeData.gioitinh}
+            onValueChange={(value) => updateField("gioitinh", value)}
+            items={[
+              { label: "Nam", value: "Nam" },
+              { label: "Nữ", value: "Nữ" },
+              { label: "Khác", value: "Khác" },
+            ]}
+            placeholder={{ label: "Chọn giới tính", value: "" }}
+            style={pickerSelectStyles} />
 
-        <TouchableOpacity onPress={() => showCalendarModal("ngaybatdau")} style={styles.datePicker}>
-          <Text style={styles.label}>Ngày vào</Text>
-          <Text>{employeeData.ngaybatdau || "Chọn ngày"}</Text>
-        </TouchableOpacity>
+          <TouchableOpacity onPress={() => showCalendarModal("ngaybatdau")} style={styles.datePicker}>
+            <Text style={styles.label}>Ngày vào</Text>
+            <Text>{employeeData.ngaybatdau || "Chọn ngày"}</Text>
+          </TouchableOpacity>
 
-        <InputField
-          label="Số điện thoại"
-          value={employeeData.sdt}
-          onChangeText={(value) => updateField("phone", value)}
-          keyboardType="phone-pad"
-        />
-        <InputField
-          label="Lương cơ bản"
-          value={employeeData.luongcoban}
-          onChangeText={(value) => updateField("luongcoban", value)}
-          keyboardType="numeric"
-        />
+          <InputField
+            label="Số điện thoại"
+            value={employeeData.sdt}
+            onChangeText={(value) => updateField("phone", value)}
+            keyboardType="phone-pad" />
+          <InputField
+            label="Lương cơ bản"
+            value={employeeData.luongcoban}
+            onChangeText={(value) => updateField("luongcoban", value)}
+            keyboardType="numeric" />
 
-        <TouchableOpacity
-          style={employeeData.trangthai ? styles.deleteButton : styles.updateButton}
-          onPress={handleToggleStatus}
-        >
-          <Text style={styles.deleteButtonText}>
-            {employeeData.trangthai ? "Ngưng hoạt động" : "Hoạt động lại"}
-          </Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            style={employeeData.trangthai ? styles.deleteButton : styles.updateButton}
+            onPress={handleToggleStatus}
+          >
+            <Text style={styles.deleteButtonText}>
+              {employeeData.trangthai ? "Ngưng hoạt động" : "Hoạt động lại"}
+            </Text>
+          </TouchableOpacity>
+{/* 
+          <TouchableOpacity style={styles.updateButton} onPress={handleUpdate}>
+            <Text style={styles.updateButtonText}>Cập Nhật</Text>
+          </TouchableOpacity> */}
 
-        <TouchableOpacity style={styles.updateButton} onPress={handleUpdate}>
-          <Text style={styles.updateButtonText}>Cập Nhật</Text>
-        </TouchableOpacity>
-
-        {showCalendar && (
-          <Calendar
-            onDayPress={handleDateSelect}
-            markedDates={{ [employeeData[dateField]]: { selected: true } }}
-          />
-        )}
-      </ScrollView>
-    </SafeAreaView>
+          {/* Modal cho Calendar */}
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={showCalendar}
+            onRequestClose={() => setShowCalendar(false)}
+          >
+            <View style={styles.modalContainer}>
+              <View style={styles.modalContent}>
+                <Calendar
+                  onDayPress={handleDateSelect}
+                  markedDates={{ [employeeData[dateField]]: { selected: true } }} />
+                <TouchableOpacity
+                  style={styles.closeButton}
+                  onPress={() => setShowCalendar(false)}
+                >
+                  <Text style={styles.closeButtonText}>Đóng</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Modal>
+        </ScrollView>
+      </SafeAreaView></>
   );
 }
 
-const InputField = ({ label, value, onChangeText }) => (
+const InputField = ({ label, ...props }) => (
   <View style={styles.inputContainer}>
     <Text style={styles.label}>{label}</Text>
-    <TextInput
-      style={styles.input}
-      value={value}
-      onChangeText={onChangeText}
-      placeholder={`Nhập ${label}`}
-    />
+    <TextInput style={styles.input} {...props} />
   </View>
 );
 
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    color: "black",
+    marginBottom: 20,
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    color: "black",
+    marginBottom: 20,
+  },
+});
+
 const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#FFF",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+
+  },
   container: {
     flex: 1,
-    backgroundColor: "#F2F2F7",
-    margin: 10,
+    padding: 20,
+    backgroundColor: "#FFFF",
   },
-  scrollView  : {
-    paddingVertical: 20,
+  scrollView: {
+    flexGrow: 1,
+    padding: 20,
+    justifyContent: "center",
   },
   avatarContainer: {
     alignItems: "center",
@@ -267,89 +306,72 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     borderRadius: 50,
-    backgroundColor: "#E3E3E3",
   },
   inputContainer: {
-    marginBottom: 15,
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    paddingLeft: 10,
+    borderRadius: 5,
   },
   label: {
     fontSize: 16,
-    color: "#333",
     marginBottom: 5,
   },
-  input: {
-    borderWidth: 1,
-    borderColor: "#DADADA",
-    padding: 10,
-    borderRadius: 5,
-    backgroundColor: "#FFF",
-  },
   datePicker: {
-    padding: 10,
+    marginBottom: 20,
+    padding: 15,
+    borderColor: "#ccc",
     borderWidth: 1,
-    borderColor: "#DADADA",
     borderRadius: 5,
-    marginBottom: 15,
-    backgroundColor: "#FFF",
-  },
-  picker: {
-    borderWidth: 1,
-    borderColor: "#DADADA",
-    padding: 10,
-    borderRadius: 5,
-    backgroundColor: "#FFF",
-    marginBottom: 15,
+    alignItems: "flex-start",
   },
   updateButton: {
-    backgroundColor: "#4CAF50",
-    paddingVertical: 15,
+    backgroundColor: "#007BFF",
+    padding: 15,
     borderRadius: 5,
     alignItems: "center",
-    marginBottom: 10,
   },
   updateButtonText: {
-    color: "#FFF",
+    color: "#FFFFFF",
     fontSize: 16,
-    fontWeight: "bold",
   },
   deleteButton: {
-    backgroundColor: "#F44336",
-    paddingVertical: 15,
+    backgroundColor: "#FF5733",
+    padding: 15,
+    marginBottom: 5,
     borderRadius: 5,
     alignItems: "center",
-    marginBottom: 10,
   },
   deleteButtonText: {
+    marginBottom: 5,
+    color: "#FFFFFF",
+    fontSize: 16,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    width: "80%",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 20,
+    alignItems: "center",
+  },
+  closeButton: {
+    marginTop: 20,
+    backgroundColor: "#4CAF50",
+    padding: 10,
+    borderRadius: 5,
+  },
+  closeButtonText: {
     color: "#FFF",
     fontSize: 16,
-    fontWeight: "bold",
   },
 });
-
-const pickerSelectStyles = StyleSheet.create({
-  inputIOS: {
-    fontSize: 16,
-    paddingVertical: 12,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: "gray",
-    borderRadius: 4,
-    color: "black",
-    paddingRight: 30,
-    backgroundColor: "#FFF",
-    marginBottom: 15,
-  },
-  inputAndroid: {
-    fontSize: 16,
-    paddingHorizontal: 10,
-    paddingVertical: 8,
-    borderWidth: 0.5,
-    borderColor: "gray",
-    borderRadius: 8,
-    color: "black",
-    paddingRight: 30,
-    backgroundColor: "#FFF",
-    marginBottom: 15,
-  },
-});
-
