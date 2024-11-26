aff4171baa2296781675b5ecd17447f52caff090
TaiThu2000 <votaithulol@gmail.com>
2024-11-26 20:11:40 +0700

    hien thi tong quan

--- app/Compoment/Dashboard.jsx
+++ app/Compoment/Dashboard.jsx
@@ -1,12 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import Icon from 'react-native-vector-icons/MaterialIcons'; // Import icon
+import Icon from "react-native-vector-icons/MaterialIcons"; // Import icon
 
 import { getPhongBanById } from "../services/InfoDataLogin";
-import {  layTatCaNhiemVu, listenForTask, layTatCaNhiemVuPhanCong, layTatCaNhiemVuPhanCongByMaNV } from "../services/Task"; // Assuming you have a service to fetch tasks
+import {
+  layTatCaNhiemVu,
+  listenForTask,
+  layTatCaNhiemVuPhanCong,
+  layTatCaNhiemVuPhanCongByMaNV,
+} from "../services/Task"; // Assuming you have a service to fetch tasks
 import { useNavigation } from "@react-navigation/native";
+import { getChamCongByMonth1 } from "../services/quanLyMucLuongFirebase";
 
-const Dashboard = ({ listEmployee, employee, onPressChamCong, onPressNhemVU }) => {
+const Dashboard = ({
+  listEmployee,
+  employee,
+  onPressChamCong,
+  onPressNhemVU,
+}) => {
   const [phongBan, setPhongBan] = useState(null);
 
   const [tasks, setTasks] = useState([]);
@@ -16,9 +27,12 @@ const Dashboard = ({ listEmployee, employee, onPressChamCong, onPressNhemVU }) =
   const [fullAllTasksNV, setFullAlTasksNV] = useState([]);
   const phongbanId = employee?.phongbanId;
   const navigation = useNavigation();
-  const isEmployee = employee?.chucvuId === 'NV';
+  const isEmployee = employee?.chucvuId === "NV";
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [dsBangCong, setDSBangCong] = useState([]);
+  const [cong, setCong] = useState({ gioLam: 0, ngayLam: 0 });
 
-  console.log(employee, "=======================")
+  console.log(employee, "=======================");
   useEffect(() => {
     const fetchPhongBan = async () => {
       if (phongbanId) {
@@ -44,12 +58,63 @@ const Dashboard = ({ listEmployee, employee, onPressChamCong, onPressNhemVU }) =
         }
       }
     };
-  
-    fetchTasks();
-  }, [employee]); 
-  
 
-  console.log(fullTasks, "Full")
+    fetchTasks();
+  }, [employee]);
+
+  useEffect(() => {
+    getChamCongByMonth1(
+      currentDate.getFullYear(),
+      currentDate.getMonth() + 1,
+      employee.employeeId,
+      (data) => {
+        setDSBangCong(data);
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    const data = dsBangCong.reduce(
+      (result, cong) => {
+        if (cong.timeIn && cong.timeOut) {
+          // Chuyển đổi timeIn và timeOut thành phút
+          const [hourIn, minuteIn] = cong.timeIn
+            .split(/[: ]/)
+            .map((v, i) => (i < 2 ? parseInt(v) : v));
+          const [hourOut, minuteOut] = cong.timeOut
+            .split(/[: ]/)
+            .map((v, i) => (i < 2 ? parseInt(v) : v));
+
+          const timeInMinutes =
+            (hourIn % 12) * 60 +
+            (minuteIn || 0) +
+            (cong.timeIn.includes("PM") ? 720 : 0);
+          const timeOutMinutes =
+            (hourOut % 12) * 60 +
+            (minuteOut || 0) +
+            (cong.timeOut.includes("PM") ? 720 : 0);
+
+          // Tính số phút làm việc trong ngày
+          const workingMinutes = timeOutMinutes - timeInMinutes;
+
+          // Cộng thêm vào tổng số giờ
+          result.gioLam += workingMinutes / 60 + cong.tangCa;
+
+          const temp = Math.min(
+            1,
+            Math.round((workingMinutes / 60 / 8) * 10) / 10
+          );
+          result.ngayLam += temp;
+        }
+        return result;
+      },
+      { gioLam: 0, ngayLam: 0 } // Khởi tạo giá trị ban đầu
+    );
+
+    setCong(data);
+  }, [dsBangCong]);
+
+  console.log(dsBangCong, "--------------------------------");
   useEffect(() => {
     const unsubscribe = listenForTask(employee.employeeId, (newTasks) => {
       setTasksNV(newTasks);
@@ -58,8 +123,11 @@ const Dashboard = ({ listEmployee, employee, onPressChamCong, onPressNhemVU }) =
       const filteredTasks = fullTasks.filter((task) =>
         newTasks.some((newTask) => newTask.manhiemvu === task.manhiemvu)
       );
-      setFullTasksNV(filteredTasks)
-      console.log(filteredTasks, '-adasd21321312 dsd       dsd    312312312sda----')
+      setFullTasksNV(filteredTasks);
+      console.log(
+        filteredTasks,
+        "-adasd21321312 dsd       dsd    312312312sda----"
+      );
 
       // Xử lý dữ liệu được lọc (ví dụ: cập nhật state khác nếu cần)
     });
@@ -71,13 +139,14 @@ const Dashboard = ({ listEmployee, employee, onPressChamCong, onPressNhemVU }) =
     };
   }, [employee, fullTasks]);
 
-
   const getTaskCount = () => {
     if (isEmployee) {
       // Nhân viên
       const matchedTasks = tasksNV.filter((nvTask) =>
         fullAllTasksNV.some(
-          (task) => task.manhiemvu === nvTask.manhiemvu && nvTask.employeeId === employee.employeeId
+          (task) =>
+            task.manhiemvu === nvTask.manhiemvu &&
+            nvTask.employeeId === employee.employeeId
         )
       );
       return matchedTasks.length > 0 ? matchedTasks.length : "Chưa có";
@@ -94,12 +163,11 @@ const Dashboard = ({ listEmployee, employee, onPressChamCong, onPressNhemVU }) =
         setFullAlTasksNV(tasks); // Cập nhật state với dữ liệu đã lấy
       });
     }
-  
+
     fetchAllAssignedTasks(); // Gọi hàm để lấy nhiệm vụ phân công
   }, []); // Chạy 1 lần khi component được mount
-  
-  console.log(fullAllTasksNV.length, "Full NV")
 
+  console.log(fullAllTasksNV.length, "Full NV");
 
   useEffect(() => {
     async function fetchTasks() {
@@ -110,17 +178,17 @@ const Dashboard = ({ listEmployee, employee, onPressChamCong, onPressNhemVU }) =
           // Now you can store the tasks in the state or process them
           setFullTasks(tasks);
         });
-  
+
         // If you want to unsubscribe later
         return unsubscribe;
       } catch (error) {
         console.error("Lỗi khi lấy nhiệm vụ phân công:", error);
       }
     }
-  
+
     fetchTasks();
   }, []);
-  
+
   return (
     <View style={styles.container}>
       {/* Phần chào hỏi */}
@@ -140,33 +208,58 @@ const Dashboard = ({ listEmployee, employee, onPressChamCong, onPressNhemVU }) =
         <View style={styles.statsContainer}>
           {/* Nhiệm vụ hôm nay */}
           <View style={[styles.statBox, styles.statBoxYellow]}>
-    <Text style={styles.statNumber}>{getTaskCount()}</Text>
-    <Text style={styles.statLabel}>Nhiệm vụ hôm nay</Text>
-    <Icon name="assignment" size={24} color="#000" style={styles.absoluteIcon} />
-  </View>
-
+            <Text style={styles.statNumber}>{getTaskCount()}</Text>
+            <Text style={styles.statLabel}>Nhiệm vụ hôm nay</Text>
+            <Icon
+              name="assignment"
+              size={24}
+              color="#000"
+              style={styles.absoluteIcon}
+            />
+          </View>
 
           {/* Nhóm của bạn */}
           <View style={[styles.statBox, styles.statBoxGreen]}>
             <Text style={styles.statNumber}>
-              {listEmployee.length} <Text style={{ fontSize: 12 }}>Thành Viên</Text>
+              {listEmployee.length}{" "}
+              <Text style={{ fontSize: 12 }}>Thành Viên</Text>
             </Text>
             <Text style={styles.statLabel}>Nhóm của bạn</Text>
-            <Icon name="group" size={24} color="#000" style={styles.absoluteIcon} />
+            <Icon
+              name="group"
+              size={24}
+              color="#000"
+              style={styles.absoluteIcon}
+            />
           </View>
 
           {/* Số giờ làm trong tháng */}
           <View style={[styles.statBox, styles.statBoxBlue]}>
-            <Text style={styles.statNumber}>Chưa có</Text>
+            <Text style={styles.statNumber}>
+              {cong.gioLam} <Text style={{ fontSize: 12 }}> Giờ</Text>
+            </Text>
             <Text style={styles.statLabel}>Số giờ làm trong tháng</Text>
-            <Icon name="access-time" size={24} color="#000" style={styles.absoluteIcon} />
+            <Icon
+              name="access-time"
+              size={24}
+              color="#000"
+              style={styles.absoluteIcon}
+            />
           </View>
 
           {/* Số ngày làm trong tháng */}
           <View style={[styles.statBox, styles.statBoxCyan]}>
-            <Text style={styles.statNumber}>Chưa có</Text>
+            <Text style={styles.statNumber}>
+              {cong.ngayLam}
+              <Text style={{ fontSize: 12 }}> Ngày</Text>
+            </Text>
             <Text style={styles.statLabel}>Số ngày làm trong tháng</Text>
-            <Icon name="calendar-today" size={24} color="#000" style={styles.absoluteIcon} />
+            <Icon
+              name="calendar-today"
+              size={24}
+              color="#000"
+              style={styles.absoluteIcon}
+            />
           </View>
         </View>
       </View>
@@ -176,9 +269,12 @@ const Dashboard = ({ listEmployee, employee, onPressChamCong, onPressNhemVU }) =
         <Text style={styles.buttonText}>Chấm công của bạn</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} onPress={onPressNhemVU => {
-        navigation.navigate("DangKyNghi", { employee: employee });
-      }}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={(onPressNhemVU) => {
+          navigation.navigate("DangKyNghi", { employee: employee });
+        }}
+      >
         <Text style={styles.buttonText}>Đăng ký nghỉ phép</Text>
       </TouchableOpacity>
 
@@ -231,10 +327,10 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     padding: 20,
     marginBottom: 15,
-    position: 'relative',
+    position: "relative",
   },
   absoluteIcon: {
-    position: 'absolute',
+    position: "absolute",
     top: 10,
     right: 10,
     opacity: 0.5,
